94ed7f7caab03c7af1f01b06d43df9f26f174f57
Fabrizio Fortunato <fortune@cortesconta.net>
2016-01-30 23:48:21 +0000

    fix lint issues

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -5,15 +5,15 @@ function copyToClipboard(text) {
   copyDiv.contentEditable = true;
   document.body.appendChild(copyDiv);
   copyDiv.innerHTML = text;
-  copyDiv.unselectable = "off";
+  copyDiv.unselectable = 'off';
   copyDiv.focus();
   document.execCommand('SelectAll');
-  document.execCommand("Copy", false, null);
+  document.execCommand('Copy', false, null);
   document.body.removeChild(copyDiv);
 }
 
-let text = (document.querySelectorAll('.target-branch .name')[0] || {}).innerText;
-if (text) {
-  console.log(text);
-  copyToClipboard(text);
+let prName = (document.querySelectorAll('.target-branch .name')[0] || {}).innerText;
+if (prName) {
+  console.log(prName);
+  copyToClipboard(prName);
 }
